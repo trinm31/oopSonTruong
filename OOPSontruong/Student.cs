f8ae9d9f94042f97a96da17713074dd53de0deda
trinm31 <trigcd19833@fpt.edu.vn>
2023-03-01 09:12:54 +0700

    inherit contructor

--- OOPSontruong/Student.cs
+++ OOPSontruong/Student.cs
@@ -2,17 +2,23 @@ using System;
 
 namespace OOPSontruong
 {
-    // public class Student : People
-    // {
-    //     public string test { get; set; }
-    //     public int Plus(int a, int b)
-    //     {
-    //         return a + b;
-    //     }
-    //     
-    //     public void Print(int a, int b = 5)
-    //     {
-    //         Console.WriteLine($"số a={a} b={b}");
-    //     }
-    // }
+    public class Student : People
+    {
+        public string Test { get; set; }
+        public Student(string test, string name, int age, string sex) 
+            : base(name,age,sex)
+        {
+            Test = test;
+        }
+        
+        public int Plus(int a, int b)
+        {
+            return a + b;
+        }
+        
+        public void Print(int a, int b = 5)
+        {
+            Console.WriteLine($"số a={a} b={b}");
+        }
+    }
 }
